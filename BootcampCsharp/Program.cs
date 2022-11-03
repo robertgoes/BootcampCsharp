78b9a19fa7e163c3a4780ec73a6389a50b0688bb
Robert <robert.goes@hotmail.com>
2022-11-03 11:31:30 -0300

    Percorrendo um array com FOREACH

--- BootcampCsharp/Program.cs
+++ BootcampCsharp/Program.cs
@@ -6,7 +6,16 @@ arrayInteiros[0] = 12;
 arrayInteiros[1] = 24;
 arrayInteiros[2] = arrayInteiros[1] * 2;
 
+Console.WriteLine("Percorrendo array com FOR:");
+
 for(int contador = 0; contador < arrayInteiros.Length; contador++)
 {
     Console.WriteLine($"Posição {contador} - {arrayInteiros[contador]}");
+}
+
+Console.WriteLine("Percorrendo array com FOREACH:");
+
+foreach(int valor in arrayInteiros)
+{
+    Console.WriteLine(valor);
 }
