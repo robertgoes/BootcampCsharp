1ac30a7de7e667c71e4b643a06900943b5e97e17
Robert <robert.goes@hotmail.com>
2022-11-03 11:50:33 -0300

    Redimensionando um Array

--- BootcampCsharp/Program.cs
+++ BootcampCsharp/Program.cs
@@ -6,16 +6,8 @@ arrayInteiros[0] = 12;
 arrayInteiros[1] = 24;
 arrayInteiros[2] = arrayInteiros[1] * 2;
 
-Console.WriteLine("Percorrendo array com FOR:");
+Console.WriteLine($"Quantidade de posições do arrayInteiros: {arrayInteiros.Length}");
 
-for(int contador = 0; contador < arrayInteiros.Length; contador++)
-{
-    Console.WriteLine($"Posição {contador} - {arrayInteiros[contador]}");
-}
+Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
 
-Console.WriteLine("Percorrendo array com FOREACH:");
-
-foreach(int valor in arrayInteiros)
-{
-    Console.WriteLine(valor);
-}
+Console.WriteLine($"Quantidade de posições do arrayInteiros: {arrayInteiros.Length}");
