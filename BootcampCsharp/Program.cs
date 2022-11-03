5e2d3629b901b71faa4a816bf45db12816a4ad56
Robert <robert.goes@hotmail.com>
2022-11-03 12:09:20 -0300

    Copiando um Array para outro

--- BootcampCsharp/Program.cs
+++ BootcampCsharp/Program.cs
@@ -6,8 +6,20 @@ arrayInteiros[0] = 12;
 arrayInteiros[1] = 24;
 arrayInteiros[2] = arrayInteiros[1] * 2;
 
-Console.WriteLine($"Quantidade de posições do arrayInteiros: {arrayInteiros.Length}");
+Console.WriteLine($"Quantidade de posições do ArrayInteiros: {arrayInteiros.Length}");
 
-Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+for(int contador = 0; contador < arrayInteiros.Length; contador++)
+{
+    Console.WriteLine($"{arrayInteiros[contador]}");
+}
 
-Console.WriteLine($"Quantidade de posições do arrayInteiros: {arrayInteiros.Length}");
+int[] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
+
+Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+
+Console.WriteLine($"Quantidade de posições do ArrayInteirosDobrado: {arrayInteirosDobrado.Length}");
+
+for(int contador = 0; contador < arrayInteirosDobrado.Length; contador++)
+{
+    Console.WriteLine($"{arrayInteirosDobrado[contador]}");
+}
