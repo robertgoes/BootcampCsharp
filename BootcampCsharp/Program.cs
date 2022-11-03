02fc72e43319b496ff025804c018f814b620917f
Robert <robert.goes@hotmail.com>
2022-11-03 10:51:13 -0300

    Implementando um array de inteiros

---
+++ BootcampCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 12;
+arrayInteiros[1] = 24;
+arrayInteiros[2] = arrayInteiros[1] * 2;
+
+for(int contador = 0; contador < arrayInteiros.Length; contador++)
+{
+    Console.WriteLine($"Posição {contador} - {arrayInteiros[contador]}");
+}
