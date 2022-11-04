3e85352acda7231aef7cb4f02d2d375dbb0942c8
Robert <robert.goes@hotmail.com>
2022-11-04 13:08:00 -0300

    Desafio de código básico C# 3 - Três Divisores

---
+++ DesafioDeCódigoBásicoCsharp/Desafio-3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*
+Dado um inteiro n, retorne true se n tiver exatamente três divisores positivos. Caso contrário, 
+retorne false.
+
+Entrada
+O arquivo de entrada consiste em um inteiro n que será validada caso esteja sob as condições do 
+desafio.
+
+Saída
+A saída consistirá em um tipo booleano: true ou false.
+*/
+
+Console.WriteLine("Informe um núemro: ");
+
+int numero = Convert.ToInt32(Console.ReadLine());
+
+int quantidadeDivisores = 0;
+
+for (int contador = 1; contador <= numero; contador++)
+{
+    if (numero % contador == 0)
+    {
+        quantidadeDivisores++;
+    }
+
+    if (quantidadeDivisores > 3)
+    {
+        Console.WriteLine(false);
+    }
+}
+
+Console.WriteLine(quantidadeDivisores == 3);
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Repositório de estudo do Bootcamp de C#
 <br>
 <br>
 [Desáfio de Código Básico C#](https://github.com/robertgoes/BootcampCsharp/blob/master/DesafioDeC%C3%B3digoB%C3%A1sicoCsharp/Program.cs)
- - Resolução do primeiro desáfio do Bootcamp.
+ - Resolução dos Desáfios de código básico do Bootcamp.
