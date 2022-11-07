2dab0940aba02ba6594cf8425a005465213b8c2b
Robert <robert.goes@hotmail.com>
2022-11-07 12:21:49 -0300

    Fila - Queue

---
+++ ExceçõesColeções/Fila - Queue/Program.cs
@@ -0,0 +1,21 @@
+﻿Queue<int> fila = new Queue<int>();
+
+for (int contador = 1; contador <= 4; contador++)
+{
+    Console.WriteLine($"Adicionando o elemento: {contador} na fila");
+    fila.Enqueue(contador);
+}
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento: {fila.Dequeue()} da fila");
+Console.WriteLine($"Adicionando o elemento: 5 na fila");
+fila.Enqueue(5);
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}
