4311ea8d8b9588deebbbf75530731961dafa099a
Robert <robert.goes@hotmail.com>
2022-11-08 20:20:15 -0300

    Operador Ternário

---
+++ TuplaOperadorTernárioDesconstrutor/OperadorTernário/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Console.WriteLine("Informe um número: ");
+
+int numero = Convert.ToInt32(Console.ReadLine());
+
+bool ehPar = numero % 2 == 0;
+
+Console.WriteLine($"O número {numero} é " + (ehPar?"par":"impar"));
