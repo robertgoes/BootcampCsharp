dd1174db1cf353dbfbfaa31c098e5b93a2ff96c1
Robert <robert.goes@hotmail.com>
2022-11-09 12:13:17 -0300

    Escrevendo um arquivo JSON

--- SerializaçãoAtributos/SerializaçãoJson/Program.cs
+++ SerializaçãoAtributos/SerializaçãoJson/Program.cs
@@ -5,4 +5,6 @@ Venda venda1 = new Venda(1, "Marca texto", 3.50M);
 
 string serializado = JsonConvert.SerializeObject(venda1, Formatting.Indented);
 
+File.WriteAllText("../Arquivos/vendas.json", serializado);
+
 Console.WriteLine(serializado);
