6f301d76d9bdd4ef7898bc4e4803950d85a94c7e
Robert <robert.goes@hotmail.com>
2022-11-09 12:24:49 -0300

    Serializando uma coleção

--- SerializaçãoAtributos/SerializaçãoJson/Program.cs
+++ SerializaçãoAtributos/SerializaçãoJson/Program.cs
@@ -2,8 +2,14 @@
 using SerializaçãoJson.Models;
 
 Venda venda1 = new Venda(1, "Marca texto", 3.50M);
+Venda venda2 = new Venda(2, "Caderno", 35.00M);
 
-string serializado = JsonConvert.SerializeObject(venda1, Formatting.Indented);
+List<Venda> listaVendas = new List<Venda>();
+
+listaVendas.Add(venda1);
+listaVendas.Add(venda2);
+
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
 
 File.WriteAllText("../Arquivos/vendas.json", serializado);
 
