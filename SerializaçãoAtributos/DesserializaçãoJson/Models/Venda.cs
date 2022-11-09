c386aa2f4bffb483be24c444cacaab3552043894
Robert <robert.goes@hotmail.com>
2022-11-09 14:04:53 -0300

    Desserialização JSON

---
+++ SerializaçãoAtributos/DesserializaçãoJson/Models/Venda.cs
@@ -0,0 +1,10 @@
+namespace DesserializaçãoJson.Models
+{
+    public class Venda
+    {
+        public int Id { get; set; }
+        public string Produto { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataVenda { get; set; }
+    }
+}
---
+++ SerializaçãoAtributos/DesserializaçãoJson/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+using DesserializaçãoJson.Models;
+
+string conteudoArquivo = File.ReadAllText("../Arquivos/vendas.json");
+
+List<Venda> listaVendas = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+
+//Serializar a lista de objeto Venda, para exibir no formato JSON
+
+string listaSerializada = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+Console.WriteLine(listaSerializada);
