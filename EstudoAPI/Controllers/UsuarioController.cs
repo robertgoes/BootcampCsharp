5552da68f29fb43e0d0c19be3f2665c705ffdfc2
Robert <robert.goes@hotmail.com>
2022-11-21 18:36:20 -0300

    API

---
+++ EstudoAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EstudoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+            
+            return Ok(obj);
+        }
+
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Olá {nome} bem vindo!";
+
+            return Ok(new {mensagem});
+        }
+    }
+}
